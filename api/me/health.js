e118571d705539085da413bd5e0a6bcf83457914
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-09-18 22:50:25 +0530

    second push

--- api/me/health.js
+++ api/me/health.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ ok: true, name: "backend", routes: "/api/*" }));
+  res.end(JSON.stringify({ ok: true }));
 }
